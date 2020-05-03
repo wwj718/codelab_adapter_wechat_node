755eda78003223f9b8f68126dc35cc9840e595eb
wwj718 <wuwenjie718@gmail.com>
2020-05-03 10:07:19 +0800

    Update my_first_node.py

--- my_first_node.py
+++ my_first_node.py
@@ -5,7 +5,7 @@ from codelab_adapter_client import AdapterNode
 class MyFirstNode(AdapterNode):
     def __init__(self):
         super().__init__()
-        self.EXTENSION_ID = "eim/wechat"
+        self.NODE_ID = "eim/extension_wechat" # 与 https://github.com/CodeLabClub/codelab_adapter_extensions/blob/master/extensions_v3/extension_wechat.py 保持相同NODE_ID，以便于与 Scratch 中的 wechat 积木兼容。
 
     def send_to_scratch(self, content):
         message = self.message_template()
